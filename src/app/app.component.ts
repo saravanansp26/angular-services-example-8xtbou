5344c436c9660fc4ac14e5864cc11964119f6ad5
saravanansp26 <saravana.nsp26@gmail.com>
2021-12-21 14:16:24 +0530

    Update comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,17 @@ import { MyServices } from './myServices';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
-  datas$:Observable<any>; ;
+  datas$: Observable<any>;
 
-  constructor(private myServices: MyServices){
-
-    this.datas$ =  this.myServices.sayHello();
+  constructor(private myServices: MyServices) {
+    //  Service initializtion
+    this.datas$ = this.myServices.sayHello();
     // Old
+    // Service subscription form component
     // this.myServices.sayHello().subscribe((data)=>{
     //   console.log(data);
     // });
